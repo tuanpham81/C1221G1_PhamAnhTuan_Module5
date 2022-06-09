9cf2cb423a6f6871acb7ca1a31515a51ae8d4782
tuanpham81 <tuanpham8196@gmail.com>
2022-06-09 09:05:54 +0700

    nop bai

--- angular-casestudy/src/app/app.module.ts
+++ angular-casestudy/src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { CustomerListComponent } from './customer-list/customer-list.component';
+import { CustomerEditComponent } from './customer-edit/customer-edit.component';
+import { CustomerCreateComponent } from './customer-create/customer-create.component';
+import { ServiceListComponent } from './service-list/service-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    CustomerListComponent,
+    CustomerEditComponent,
+    CustomerCreateComponent,
+    ServiceListComponent
   ],
   imports: [
     BrowserModule,
