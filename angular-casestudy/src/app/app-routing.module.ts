dc7efe0d9b8d56645a102d63f3e8b70ab958104d
tuanpham81 <tuanpham8196@gmail.com>
2022-06-12 20:18:43 +0700

    nop bai

--- angular-casestudy/src/app/app-routing.module.ts
+++ angular-casestudy/src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomepageComponent} from './homepage/homepage.component';
+import {CustomerListComponent} from './customer-management/customer-list/customer-list.component';
+import {CustomerEditComponent} from './customer-management/customer-edit/customer-edit.component';
+import {CustomerCreateComponent} from './customer-management/customer-create/customer-create.component';
+import {FacilityListComponent} from './faiclity-management/facility-list/facility-list.component';
+import {FacilityEditComponent} from './faiclity-management/facility-edit/facility-edit.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'homepage'},
+  {path: 'homepage', component: HomepageComponent},
+  {path: 'customer-list', component: CustomerListComponent},
+  {path: 'customer-edit', component: CustomerEditComponent},
+  {path: 'customer-create', component: CustomerCreateComponent},
+  {path: 'facility-list', component: FacilityListComponent},
+  {path: 'facility-edit', component: FacilityEditComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
